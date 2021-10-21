3cd5fa3914dd2adeda1d459271822cc2552f768b
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-21 11:27:59 +0200

    css

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import { recipes } from "./recipes.js";
 console.log(recipes);
 
 // // display all recipes
-
 function ingredientsFun(ingredients){
   return`
                      <ul class="ingredients-list">
@@ -13,37 +12,46 @@ function ingredientsFun(ingredients){
                           `
                         }).join("")}
                      </ul>
-  `
-}
+  `}
 
-
+// display recipesBox
 function recipiesBox(recipe) {
     return `
             <div class="recepiesBox">
             <div class="recepiesimg"> PHOTO</div>
-
                 <div class="recepiesinfo">
-
                   <div class="nameTime">
                     <h3 class="name">${recipe.name}</h3>
                     <h3 class="time"><i class="far fa-clock"></i>${recipe.time} min</h3>
                   </div>
-
                     <div class="ingDes">
-
                     ${ingredientsFun(recipe.ingredients)}
-                  
                     <p class="description">${recipe.description}</p>
                     </div>
                 </div>
             </div>
- `
-};
-
+ `};
 document.getElementById("recepies").innerHTML = `
  ${recipes.map(recipiesBox).join("")}
   `;
 
+
+// buttons 
+function buttonsSelect () {
+  return `
+
+  <div class="ingredient"><p>Ingredient <i class="fas fa-angle-down"></i></p></div>
+  <div class="ingredient-hide" id="ingredient-hide"></div>
+ 
+  <div class="appareil"><p>Appareil <i class="fas fa-angle-down"></i></p></div>
+  <div class="appareil-hide" id="appareil-hide"></div>
+
+  <div class="ustensiles"><p>Ustensiles <i class="fas fa-angle-down"></i></p></div>
+  <div class="ustensiles-hide" id="ustensiles-hide"></div>
+
+  `
+};
+document.getElementById("buttons-select").innerHTML =  buttonsSelect() ;
 
   
 // Ingredient button
@@ -52,7 +60,7 @@ console.log(ingredientBtn);
 
 function ingredient(recipe) {
   return `
-  <div class="appliance-list">
+  <div class="ingredient-list">
    <p>  ${ingredientsFun(recipe.ingredients)} </p>
   </div>
 `
@@ -62,6 +70,9 @@ ingredientBtn.innerHTML = `
 ${recipes.map(ingredient).join("")}
 `;
 
+
+// show Ingredient list on click
+ 
 
 
 // Appareil button
@@ -96,3 +107,5 @@ function ustensiles(recipe) {
 ustensilesBtn.innerHTML = `
 ${recipes.map(ustensiles).join("")}
 `;
+
+
