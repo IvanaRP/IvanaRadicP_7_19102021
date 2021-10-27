11993d5d88b759882c17497a07e69d55441903e6
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-28 00:13:42 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,13 +8,11 @@ let ingredients;
 let appareil;
 // let ustensiles;
 
-// // Search Input
+// // Search Input 
 let inputSearch = document.getElementById("searchBar");
   console.log(inputSearch);
 
-
-
-
+  // Search Input  by input
   inputSearch.addEventListener("keyup", function(e){
     //   console.log(e.target.value);
         let searchItem = e.target.value.toLowerCase();
@@ -44,10 +42,6 @@ let inputSearch = document.getElementById("searchBar");
 // recepiesCards
 let recepiesCard = document.getElementById("recepies");
 console.log(recepiesCard);
-
-// // search bar
-// const searchBar = document.getElementById("searchBar");
-// console.log(searchBar);
 
 
 //display all Ustensiles
@@ -167,53 +161,4 @@ ustensilsBtn.innerHTML = `${recipes.map(ustensils).join("")}`;
 
 
 
-
-// // filter SEARCH input
-// let inputSearch = document.getElementById("searchBar");
-//   console.log(inputSearch);
-
-//   inputSearch.addEventListener('keyup', function(e){
-//       const term = e.target.value.toLowerCase();
-//     //   console.log(term);
-//     const books = document.getElementById("name");
-//     console.log(books);
-//     Array.from(books).forEach(function(book){
-//         const title = book.textContent;
-//         console.log(title);
-//         if(title.toLowerCase().indexOf(term)!= -1){
-//             book.style.display = 'block';
-//         }else {
-//             book.style.display = 'none';
-//         }
-//     })
-//   })
-
-
-
-
-
-// // Search Input
-// let inputSearch = document.getElementById("searchBar");
-//   console.log(inputSearch);
-
-//   inputSearch.addEventListener("keyup", function(){
-//       let value = this.value;
-//       console.log('Value:', value);
-//       let data = searchRecepies(value, recipes)
-//       recipiesBox(recipes);
-
-
-//   });
-
-//   function searchRecepies(value, recipes){
-//       let filteredRecepies = [];
-//         for(var i = 0; i < recipes.length; i++)
-//         value = value.toLowerCase()
-//         let name = recipes[i].name.toLowerCase()
-       
-
-//             if (name.includes(value)){
-//                 filteredRecepies.push(data[i])
-//             }
-//         }
 
