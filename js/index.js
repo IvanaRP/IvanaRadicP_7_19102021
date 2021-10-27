88744956e0f6870b5a7dcee3b7777db90f6482ab
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-27 19:43:25 +0200

    filter input
    Search Input by ingredient name etc

--- js/index.js
+++ js/index.js
@@ -8,13 +8,46 @@ let ingredients;
 let appareil;
 // let ustensiles;
 
+// // Search Input
+let inputSearch = document.getElementById("searchBar");
+  console.log(inputSearch);
+
+
+
+
+  inputSearch.addEventListener("keyup", function(e){
+    //   console.log(e.target.value);
+        let searchItem = e.target.value.toLowerCase();
+        let items = document.querySelectorAll(".recipeinput");
+        console.log(items);
+        // select whole div
+
+        // let recepiescar = document.getElementById("recepiesBox");;
+        // console.log(recepiescar);
+
+        items.forEach(function(item){
+            console.log(item.textContent);
+            if(item.textContent.toLowerCase().indexOf(searchItem) != -1){
+                item.closest("li").style.display = "block";
+                console.log(item.closest("li"));
+            }
+            else {
+                item.closest("li").style.display = "none";
+            }
+        })
+
+        console.log(searchItem);
+  })
+
+
+
 // recepiesCards
 let recepiesCard = document.getElementById("recepies");
 console.log(recepiesCard);
 
-// search bar
-const searchBar = document.getElementById("searchBar");
-console.log(searchBar);
+// // search bar
+// const searchBar = document.getElementById("searchBar");
+// console.log(searchBar);
 
 
 //display all Ustensiles
@@ -46,18 +79,19 @@ function ingredientsFun(ingredients) {
             </ul>
       `;
 }
-// ${ingredientsFun(recipe.ingredients)} 
+
 // draw recepiesCard
 function recipiesBox(recipe) {
-  return `<div class="recepiesBox">
+  return `<li class="recipeinput" id="recipeinput">
+              <div class="recepiesBox" id="recipeinput">
                 <div class="recepiesimg"> PHOTO</div>
                     <div class="recepiesinfo">
                       <div class="nameTime">
-                        <h3 class="name">${recipe.name}</h3>
-                        <h3 class="time"><i class="far fa-clock"></i>${
-                          recipe.time
-                        } min</h3>
+                        <h3 class="name" id="name">${recipe.name}</h3>
+                        <h3 class="time"><i class="far fa-clock"></i>${recipe.time} min</h3>
                       </div>
+
+                   
                         <div class="ingDes">
 
                         ${ingredientsFun(recipe.ingredients)}  
@@ -67,6 +101,7 @@ function recipiesBox(recipe) {
                         </div>
                     </div>
                 </div>
+         </li>
     `;
 }
 recepiesCard.innerHTML = `${recipes.map(recipiesBox).join("")}`;
@@ -131,4 +166,54 @@ function ustensils(recipe) {
 ustensilsBtn.innerHTML = `${recipes.map(ustensils).join("")}`;
 
 
+
+
+// // filter SEARCH input
+// let inputSearch = document.getElementById("searchBar");
+//   console.log(inputSearch);
+
+//   inputSearch.addEventListener('keyup', function(e){
+//       const term = e.target.value.toLowerCase();
+//     //   console.log(term);
+//     const books = document.getElementById("name");
+//     console.log(books);
+//     Array.from(books).forEach(function(book){
+//         const title = book.textContent;
+//         console.log(title);
+//         if(title.toLowerCase().indexOf(term)!= -1){
+//             book.style.display = 'block';
+//         }else {
+//             book.style.display = 'none';
+//         }
+//     })
+//   })
+
+
+
+
+
+// // Search Input
+// let inputSearch = document.getElementById("searchBar");
+//   console.log(inputSearch);
+
+//   inputSearch.addEventListener("keyup", function(){
+//       let value = this.value;
+//       console.log('Value:', value);
+//       let data = searchRecepies(value, recipes)
+//       recipiesBox(recipes);
+
+
+//   });
+
+//   function searchRecepies(value, recipes){
+//       let filteredRecepies = [];
+//         for(var i = 0; i < recipes.length; i++)
+//         value = value.toLowerCase()
+//         let name = recipes[i].name.toLowerCase()
+       
+
+//             if (name.includes(value)){
+//                 filteredRecepies.push(data[i])
+//             }
+//         }
 
